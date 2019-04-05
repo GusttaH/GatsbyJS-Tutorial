d34934043acb68545a6419e14e2f241b9879d469
= <contato.gustta.H@hotmail.com>
2019-04-04 23:46:12 -0300

    Layout com Gatsby

---
+++ Gatsby---tutorial-part-three/src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+export default () =>(
+    <Layout>
+        <h1>About me</h1>
+        <p>I'm good enough, I'm Smart Enough, and gosh darn it, people like!</p>
+    </Layout>
+)
---
+++ Gatsby---tutorial-part-three/src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+export default () =>(
+    <Layout>
+        <h1>Eu adoraria conversar! Me envie um email no endeço abaixo.</h1>
+        <p>
+            <a href="mailto:me@example.com">me@exemple.com</a>
+        </p>
+    </Layout>
+)
