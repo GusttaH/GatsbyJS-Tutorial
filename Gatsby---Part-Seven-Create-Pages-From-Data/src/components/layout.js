c62f6a7dc12685b74f66f74ebb83919f5dac0bf3
= <contato.gustta.H@hotmail.com>
2019-04-07 15:18:21 -0300

    Creating Pages From data using GraphQL

---
+++ Gatsby---Part-Seven-Create-Pages-From-Data/src/components/layout.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { css } from "@emotion/core"
+import { StaticQuery, Link, graphql} from "gatsby"
+
+import {rhythm} from "../utils/typography"
+
+export default ({children}) => (
+    <StaticQuery
+        query={graphql`
+            query{
+                site{
+                    siteMetadata{
+                        title
+                    }
+                }
+            }
+          `   
+        }       
+            
+                
+render={data => (
+    <div 
+        css={css`
+                padding-top: ${rhythm(1.5)};
+                margin     : o auto;
+                pax-width  : 700px;
+                padding    : ${rhythm(2)};
+            `}
+    >
+      <Link to={'/'}>
+        <h3
+            css={css`
+                margin-bottom: ${rhythm(2)};
+                font-style   : normal;
+                display      : inline-block;
+            `}
+         >
+            {data.site.siteMetadata.title}
+         </h3>   
+      </Link>  
+      <Link to={'/about/'}
+            css={css`
+                float: right;
+            `}
+      >
+        About
+    </Link>   
+    <Link to={'/my-files'}css={css`
+            float: right;
+            margin-right: 1.5rem;
+          `}
+    >My Files</Link>    
+
+    <Link to={'/'}css={css`
+            float: right;
+            margin-right: 1.5rem;
+            `}
+    >Home</Link>
+        {children}
+    </div>
+    
+    )}
+/>
+)
